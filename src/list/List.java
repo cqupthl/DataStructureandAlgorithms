9cc1d080244a8eafe325235f6757b3976cffc3e2
hl123 <1956286912@qq.com>
2019-09-03 12:29:43 +0800

    1

--- src/list/List.java
+++ src/list/List.java
@@ -4,7 +4,7 @@ public class List {
     Node head=null;
 
     /*
-      遍历节点与长度
+      遍历节点
      */
    public void sysNode(){
        Node temp=head;
@@ -12,7 +12,7 @@ public class List {
            if(temp.next==null){
                System.out.println(temp.toData());
            }else{
-               System.out.println(temp.toData()+"---");
+               System.out.print(temp.toData()+"---");
            }
            temp=temp.next;
        }
@@ -21,17 +21,41 @@ public class List {
     /*增加节点
     */
     public void addNode(int num){
-        Node newNode=new Node(1);
+        Node newNode=new Node(num);
         if(head==null){
             head=newNode;
         }else{
             Node temp=head;
-            while(temp!=null){
+            while(temp.next!=null){
                 temp=temp.next;
             }
-            temp=newNode;
+            temp.next=newNode;
         }
 
     }
-
+  /*长度
+  * */
+  public int length(){
+      Node temp=head;
+      int length=0;
+      while(temp!=null){
+          length++;
+          temp=temp.next;
+      }
+      return length;
+  }
+  /*按值查找*/
+  public boolean findNode(int num){
+      Node temp=head;
+      if(head==null){
+          return false;
+      }
+      while(temp!=null){
+          if(num==temp.data){
+              return true;
+          }
+          temp=temp.next;
+      }
+      return false;
+  }
 }
--- src/list/Test.java
+++ src/list/Test.java
@@ -1,10 +1,18 @@
 package list;
 
+
+
 public class Test {
     public static void main(String[] args) {
         List list=new List();
         list.addNode(1);
         list.addNode(2);
-        list.sysNode();
+//        list.sysNode();
+//        System.out.println(list.length());
+        System.out.println(list.findNode(1));
     }
 }
+
+
+
+
